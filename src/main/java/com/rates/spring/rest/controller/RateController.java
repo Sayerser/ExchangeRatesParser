5004f6562dbcc8cfefa0270b554c405d133025e1
Danil Golubenkov <Golubenkow2004@mail.ru>
2024-07-07 19:11:58 +0300

    feat[5]: deleted springfox dependencies, added springdoc

--- src/main/java/com/rates/spring/rest/controller/RateController.java
+++ src/main/java/com/rates/spring/rest/controller/RateController.java
@@ -3,8 +3,8 @@ package com.rates.spring.rest.controller;
 import com.rates.spring.rest.dto.response.RateResponse;
 import com.rates.spring.rest.entity.RateEntity;
 import com.rates.spring.rest.service.RateService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,7 +13,7 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("v1/api/rates")
-@Api(value = "Rate Management System")
+@Tag(name = "Rate Management System", description = "APIs for managing rates")
 public class RateController {
 
     private final RateService rateService;
@@ -24,7 +24,7 @@ public class RateController {
     }
 
     @GetMapping("/{id}")
-    @ApiOperation(value = "Get rate by ID")
+    @Operation(summary = "Get rate by ID", description = "Retrieve a rate by its unique identifier")
     public ResponseEntity<RateResponse> getRateById(@PathVariable UUID id) {
         RateEntity rateEntity = rateService.getRateById(id);
         RateResponse rateResponse = convertToResponse(rateEntity);
@@ -41,4 +41,3 @@ public class RateController {
         return rateResponse;
     }
 }
-
